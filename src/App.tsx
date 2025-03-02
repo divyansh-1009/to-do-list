8ee36d754823db895e29f066356732ab51468abb
Divyansh Yadav <anshmcs@gmail.com>
2025-03-02 18:18:24 +0530

    added textbox and add button with some basic CSS functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import ToDoList from './to-do-list';
 function App(){
-
+return (
+  <div><ToDoList></ToDoList></div>
+)
 }
 export default App;
--- src/to-do-list.tsx
+++ src/to-do-list.tsx
@@ -6,7 +6,11 @@ function ToDoList(){
 
     }
     return (<>
-    
+    <div><h1 className="heading">TO DO LIST</h1></div>
+    <div className="container">
+    <input className="input" type="text" placeholder="Enter your new task...."></input>
+    <button className="addButton" onClick={addTask}>Add Task</button>
+    </div>
     </>)
 }
 export default ToDoList;
