4d881b9210e79beeeaeead6e727f1d6839058a5f
Divyansh Yadav <anshmcs@gmail.com>
2025-03-03 18:15:10 +0530

    final code with add button, delete button, ordered list of all the tasks  being displayed and background image

--- src/to-do-list.tsx
+++ src/to-do-list.tsx
@@ -1,16 +1,31 @@
+import { useState } from "react";
 function ToDoList(){
-    function addTask(){
-
+    let [newTask,setNewTask]=useState("");
+    let [tasks,setTasks]=useState<string[]>([]); 
+    function input(task){
+        setNewTask(task.target.value);
     }
-    function removeTask(){
-
+    function addTask(){
+        setTasks(tasks=>[...tasks,newTask]);
+        setNewTask("");
+    }
+    function removeTask(index){
+        let updatedTasks=tasks.filter((tasks,i)=>i!==index);
+        setTasks(updatedTasks);
     }
     return (<>
     <div><h1 className="heading">TO DO LIST</h1></div>
     <div className="container">
-    <input className="input" type="text" placeholder="Enter your new task...."></input>
+    <input className="input" type="input-text" placeholder="Enter your new task...." value={newTask} onChange={input}></input>
     <button className="addButton" onClick={addTask}>Add Task</button>
     </div>
+    <div className="list"><ol>
+            {tasks.map((tasks,index)=>
+                        <li key={index}><span className="output-task">{tasks}</span>
+                        <button onClick={()=>removeTask(index)} className="deleteButton">Delete task</button>
+                        </li>
+            )}
+    </ol></div>
     </>)
 }
 export default ToDoList;
